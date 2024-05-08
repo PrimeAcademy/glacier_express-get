65ddfdf6675dd97d7037792f44fe784d22c85517
Mater Dev <aryeh.the.lion@gmail.com>
2024-05-08 14:52:18 -0500

    update client with get request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,19 @@
 
 let getQuotes = () => {
     console.log("getQuotes is working!")
-
+    axios({
+        method: 'GET', // HTTP method
+        url: '/canipetthatdawg'
+    })
+    .then((response) => { // Captures the response from server
+        // Must be response.data
+        let quoteList = response.data
+        console.log("quoteList", quoteList)
+    })
+    .catch((error) => { // Manages errors
+        console.log("GET for /quotes didnt work...", error)
+        alert("Oopsie, that didnt work.")
+    })
     // Use axios to make a GET request
         // then...
             // Render to the DOM
@@ -13,7 +25,14 @@ let getQuotes = () => {
 // * Will call function to retrieve quotes the first time the page loads.
 getQuotes()
 
-let renderQuotes = () => {
-    console.log("renderQuotes() is working...")
+// Will recieve data to render to DOM
+    // arg: allQuotes (array of objects)
+let renderQuotes = (allQuotes) => {
+    console.log("renderQuotes() is working... quotes:", allQuotes)
+
+    // loop through array
+        // For each quote will render a <li> with quote and author...
+            // <li> "quote....", - author </li> 
+
 }
 
