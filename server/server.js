44d74c7ff35f6367663510fa886962b1b84063cb
Mater Dev <aryeh.the.lion@gmail.com>
2024-05-08 10:00:11 -0500

    add get route

--- server/server.js
+++ server/server.js
@@ -11,6 +11,31 @@ const port = 5000
   // Tell the server where the files for the page are
 app.use(express.static('server/public'))
 
+// -------- GET ROUTE ------------------
+
+let quoteList = [
+  { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+  { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+  { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+
+// * GET Route to return some quotes
+  // A method type, which receives 2 arguments
+    // - A path
+    // - Callback function (arrow function)
+  // All routes on a server must respond with something! (A status code, or data)
+app.get('/quotes', (req, res) => {
+  // Write the code for the endpoint
+
+  // * Sending some data
+  res.send(quoteList)
+
+  // * Sending a status number:
+  // res.sendStatus(500)
+})
+
+// -------- GET ROUTE END ---------------
+
 // ! Actually start server when file is ran
 app.listen(
     port,
