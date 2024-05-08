c7a5e47c76e6cf453857ad4d6c7160fef0e9ada7
Mater Dev <aryeh.the.lion@gmail.com>
2024-05-08 14:39:09 -0500

    setup comments for client.js

---
+++ server/public/scripts/client.js
@@ -0,0 +1,19 @@
+// ! Write a function that will retrieve the data on the `/quotes` GET endpoint
+
+let getQuotes = () => {
+    console.log("getQuotes is working!")
+
+    // Use axios to make a GET request
+        // then...
+            // Render to the DOM
+        // catch
+            // any error and provide an alert
+}
+
+// * Will call function to retrieve quotes the first time the page loads.
+getQuotes()
+
+let renderQuotes = () => {
+    console.log("renderQuotes() is working...")
+}
+
